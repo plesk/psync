f164c2673712d7820724d9692eb261be0b1b40a6
Alexey Yuzhakov <sibprogrammer@gmail.com>
2026-07-10 22:54:38 +0100

    Add support for Plesk extensions

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"encoding/json"
+	"encoding/xml"
 	"log"
 	"os"
 	"os/exec"
@@ -13,10 +15,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var mappingRules = map[string]string{
+var pleskMappingRules = map[string]string{
 	"common/php/plib":    "/usr/local/psa/admin/plib",
 	"common/php/htdocs":  "/usr/local/psa/admin/htdocs",
 	"common/application": "/usr/local/psa/admin/application",
+}
+
+var pleskExtensionMappingRules = map[string]string{
+	"src/plib":   "/usr/local/psa/admin/plib/modules",
+	"src/htdocs": "/usr/local/psa/admin/htdocs/modules",
 }
 
 var ignorePatterns = []string{"*~", ".*.sw?", "*.tmp", ".DS_Store", "Thumbs.db"}
@@ -62,7 +69,100 @@ func uploadFile(eventPath string, sourcePath string, targetPath string) {
 	log.Printf("Uploaded file %s -> %s:%s", eventPath, remoteHost, targetFullPath)
 }
 
+func fileExists(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
+	}
+	return true
+}
+
+func isPleskComposer() bool {
+	if !fileExists("composer.json") {
+		return false
+	}
+
+	data, err := os.ReadFile("composer.json")
+	if err != nil {
+		return false
+	}
+
+	var m map[string]any
+	if err := json.Unmarshal(data, &m); err != nil {
+		return false
+	}
+
+	if m["name"] == "plesk/plesk" {
+		return true
+	}
+
+	return false
+}
+
+func getPleskExtensionName(extensionMetaFile string) string {
+	f, err := os.Open(extensionMetaFile)
+	if err != nil {
+		return ""
+	}
+	defer f.Close()
+
+	dec := xml.NewDecoder(f)
+	depth := 0
+	for {
+		tok, err := dec.Token()
+		if err != nil {
+			break
+		}
+		switch t := tok.(type) {
+		case xml.StartElement:
+			depth++
+			if depth == 2 && t.Name.Local == "id" {
+				var v string
+				_ = dec.DecodeElement(&v, &t)
+				return v
+			}
+		case xml.EndElement:
+			depth--
+		}
+	}
+
+	return ""
+}
+
+func getMappingRules() map[string]string {
+	if isPleskComposer() {
+		log.Println("Plesk detected")
+		return pleskMappingRules
+	}
+
+	extensionMetaFile := filepath.Join("src", "meta.xml")
+
+	if fileExists(extensionMetaFile) {
+		extensionName := getPleskExtensionName(extensionMetaFile)
+		if extensionName == "" {
+			return nil
+		}
+
+		for rule, value := range pleskExtensionMappingRules {
+			pleskExtensionMappingRules[rule] = filepath.Join(value, extensionName)
+		}
+
+		log.Printf("Plesk extension %s detected", extensionName)
+		return pleskExtensionMappingRules
+	}
+
+	return nil
+}
+
 func runWatcher() {
+	mappingRules := getMappingRules()
+
+	if mappingRules == nil {
+		log.Println("Unknown source tree, exiting...")
+		return
+	}
+
 	dev, _ := fsevents.DeviceForPath(".")
 	es := &fsevents.EventStream{
 		Paths:   []string{"."},
